bddeb902777fc68b236751a454900b536a48a9c2
aliaasayed <aliaasayed45@gmail.com>
2019-10-05 16:22:58 +0200

    contacts and user dummy data

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var server = express();
 
+server.use(function (req, resp, next) {
+  resp.header("Access-Control-Allow-Origin", "*");
+  resp.header("Access-Control-Allow-Headers", "Content-Type");
+  resp.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  next();
+});
 
-server.listen(3000,function(){
+var contactRouter = require("./controllers/contacts");
+server.use("/contacts", contactRouter);
+
+server.listen(3000, function () {
   console.log("Starting listen on port 3000...");
 });
